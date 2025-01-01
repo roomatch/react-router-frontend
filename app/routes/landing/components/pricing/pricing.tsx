14db0b5a63282f33536db7334ad27a8af68464fd
sfrancop <s.francop@uniandes.edu.co>
2025-01-01 17:15:49 -0500

    feat: add id to plans section
    related to KAN-183

--- app/routes/landing/components/pricing/pricing.tsx
+++ app/routes/landing/components/pricing/pricing.tsx
@@ -3,7 +3,7 @@ import Accordion from "./components/accordion/accordion";
 
 export default function Pricing() {
     return (
-        <Flex direction="column" align="center" justify="center" gap="3" className="p-6 max-w-[1024px] mx-auto">
+        <Flex id="planes" direction="column" align="center" justify="center" gap="3" className="p-6 max-w-[1024px] mx-auto">
             <Heading as="h1" align="center" className="max-w-[768px]">Encuentra el plan perfecto para ti, elige ahora y paga solo si concretamos tu match</Heading>
             <Text as="p" align="center">Elige un plan asequible y diseñado con las mejores herramientas para conectar con roomies compatibles, asegurar confianza en el proceso, y facilitar la búsqueda de tu próximo hogar.</Text>
             <Accordion />
