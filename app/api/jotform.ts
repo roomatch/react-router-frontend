c1b68bb1fc0f18d39fa566b55293be33106339d7
JackRakham <j.villatet@uniandes.edu.co>
2024-12-31 19:27:04 -0500

    refactor: :fire: Remove get submissions method from persistence
    Removed ge Submission method from persistence since it's no longer used
    
    KAN-197

--- app/api/jotform.ts
+++ app/api/jotform.ts
@@ -6,17 +6,6 @@ import { createRoomieArrendador, type RoomieArrendador } from "./model/roomieArr
 
 const API_KEY = process.env.JOTFORM_APIKEY
 
-export async function getSubmissions(): Promise<JotFormResponse> {
-    try {
-        const response = await fetch(`https://api.jotform.com/form/243607063936662/submissions?apiKey=${API_KEY}`);
-        const data: JotFormResponse = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching submissions:", error);
-        throw error;
-    }
-}
-
 export async function editSubmission(questionId: string, newAnswer: string, submissionId: string) {
     try {
         const requestBody = {
@@ -67,73 +56,4 @@ export async function getRoomieArrendador(celular: string) {
     const submission = getSubmission(roomieArrendadorInfo.submission_id)
     const roomieArrendador = createRoomieArrendador(submission)
     return roomieArrendador
-}
-
-async function updateMatrix() {
-    try {
-        const submissions = await getSubmissions()
-        const currentUsersInfo = await getUsersInfo()
-        let roomies: Roomie[] = []
-        let roomiesArrendadores: RoomieArrendador[] = []
-        submissions.content.forEach((submission) => {
-            console.log(`Submission ID: ${submission.id}`);
-            if (submission["answers"]["82"].answer == "Solo busco habitación, pero igual quiero tener buenos roomies") {
-                const roomie = createRoomie(submission)
-                roomies.push(roomie)
-                console.log(roomie)
-            } else {
-                const roomieArrendador = createRoomieArrendador(submission)
-                roomiesArrendadores.push(roomieArrendador)
-                console.log(roomieArrendador)
-            }
-        });
-        for (const roomie of roomies) {
-            await findOrAddUser(roomie.celular, "roomie", [], "");
-            let compatibles = []
-            for (const roomieArrendador of roomiesArrendadores) {
-                await findOrAddUser(roomieArrendador.celular, "roomieArrendador", [], "")
-                let compatibilidad = 0
-                //Check localidad
-                const roomieLocalidadesSet = new Set(roomie.localidadesBuscadas);
-                const hasCommonElement = roomieArrendador.localidadVivienda.some(localidad =>
-                    roomieLocalidadesSet.has(localidad)
-                );
-                if (hasCommonElement) compatibilidad += 50;
-                //Check presupuesto
-                if (roomie.rangoPresupuestoMin > roomieArrendador.precioHabitacion &&
-                    roomie.rangoPresupuestoMax <= roomieArrendador.precioHabitacion) compatibilidad += 30
-                //Check amoblada
-
-                //Check genero
-                if (roomie.generoPreferencia = "Sí, solo quiero vivir con hombres") {
-                    if (roomieArrendador.genero != "Masculino" && roomieArrendador.generoApartamento != "Hombres") compatibilidad = 0
-                } else if (roomie.generoPreferencia = "Sí, solo quiero vivir con mujeres") {
-                    if (roomieArrendador.genero != "Femenino" && roomieArrendador.generoApartamento != "Mujeres") compatibilidad = 0
-                } else {
-                    compatibilidad += 20
-                }
-                //Check edad
-                if (roomieArrendador.edad < roomie.rangoEdadRoomieMin || roomieArrendador.edad >= roomie.rangoEdadRoomieMax) compatibilidad = 0
-                //Check animales
-                if (roomie.animalesMolestia == "SÍ" && roomieArrendador.viviraConMascota == "Sí") compatibilidad = 0;
-                //Check orden
-                const mapeoOrden = {
-
-                }
-                //Check limpieza
-                const mapeoLimpieza = {
-
-                }
-                //Check ruido
-                const mapeoRuido = {
-
-                }
-
-                if (compatibilidad > 50) compatibles.push(roomieArrendador.celular)
-            }
-            await updateUserCompatibles(roomie.celular, compatibles)
-        }
-    } catch (error) {
-        console.error("Error fetching submissions:", error);
-    }
 }
